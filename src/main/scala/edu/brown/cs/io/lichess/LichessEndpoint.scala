30d491273223286d1516a8a7938653dfecb7b9b3
Joe Han <qiyu_han@brown.edu>
2020-08-31 01:00:04 -0400

    configured for repl-free run

--- src/main/scala/edu/brown/cs/io/lichess/LichessEndpoint.scala
+++ src/main/scala/edu/brown/cs/io/lichess/LichessEndpoint.scala
@@ -12,29 +12,30 @@ import edu.brown.cs.chessgame.{GameCommands, GameState}
 import scala.util.{Failure, Success, Try}
 import chess.Move
 import edu.brown.cs.engine.{AlphaBeta, AlphaBetaEngine}
-import edu.brown.cs.io.ModelTranslations
+import edu.brown.cs.io.{ChessLogger, ModelTranslations}
 
 class LichessEndpoint(token: String, botId: String, server: String) extends ModelTranslations {
 
   val DEFAULT_READ_TIMEOUT = 1800000
   val DEFAULT_CONNECT_TIMEOUT = 30000
 
-  val activeGame : AtomicBoolean = new AtomicBoolean(false)
-  val joinedGame : AtomicBoolean = new AtomicBoolean(false)
-  val gameStateExists : AtomicBoolean = new AtomicBoolean(false)
+  private val activeGame : AtomicBoolean = new AtomicBoolean(false)
+  private val joinedGame : AtomicBoolean = new AtomicBoolean(false)
+  private val gameStateExists : AtomicBoolean = new AtomicBoolean(false)
+  private val connectionOpen : AtomicBoolean = new AtomicBoolean(true)
   private var opponentColor = "white"
-  var gameState : GameState = null
-  var gameId : String = ""
-  var engine : AlphaBetaEngine = null;
+  private var gameState : GameState = null
+  private var gameId : String = ""
+  private var engine : AlphaBetaEngine = null;
 
   implicit val ec = ExecutionContext.global
 
   def upgradeToBot(args: Vector[String]): Unit ={
     val response: HttpResponse[String] = Http(s"${server}/api/bot/account/upgrade").header("Authorization", s"Bearer $token").postForm.asString
     response.code match {
-      case 200 => println(s"Success: ${response.body}")
-      case 429 => println("Rate limit"); Thread.sleep(60000)
-      case _ => println(s"Error: ${response.body}")
+      case 200 => ChessLogger.info(s"Success: ${response.body}")
+      case 429 => ChessLogger.warn("Rate limit"); Thread.sleep(60000)
+      case _ => ChessLogger.error(s"Error: ${response.body}")
     }
   }
 
@@ -46,57 +47,73 @@ class LichessEndpoint(token: String, botId: String, server: String) extends Mode
             strm => {
               scala.io.Source.fromInputStream(strm).getLines().foreach(processEvent)
             }).code match {
-            case 200 => println("event received OK")
-            case 429 => println("rate limit req"); Thread.sleep(60000)
-            case _ => println("error - other status code")
+            case 200 => ChessLogger.debug("event received OK")
+            case 429 => ChessLogger.warn("rate limit req"); Thread.sleep(60000)
+            case _ => ChessLogger.error("error - other status code")
           }
         } catch {
-          case ex: Exception => println("Event Processing Error: " + ex.getMessage); ex.printStackTrace(); Thread.sleep(60000)
+          case ex: Exception => ChessLogger.error("Event Processing Error: " + ex.getMessage); ex.printStackTrace(); Thread.sleep(60000)
         }
       }
-      "Event stream closed normally"
+      "Event stream closed"
     } onComplete {
-      case Success(r) => println(r)
-      case Failure(ex) => println("Event Future error: "  + ex.getMessage)
+      case Success(r) =>
+        ChessLogger.info(r)
+        connectionOpen.set(false)
+      case Failure(ex) =>
+        ChessLogger.error("Event Future error: "  + ex.getMessage)
+        connectionOpen.set(false)
     }
+  }
+
+  def isConnectionOpen(): Boolean ={
+    connectionOpen.get()
   }
 
   private def processEvent(str: String): Unit ={
     if(!str.isBlank) {
-      println(str)
+      ChessLogger.debug(str)
       val event = getInboundEvent(str)
       event.`type` match {
-        case Some("challenge") => println(s"received challenge ${event.challenge.getOrElse("ex get chal")}"); processChallenge(event.challenge.get)
-        case Some("gameStart") => println("game started"); processGame(event.game.get.id)
-        case _ => println("event not recognized")
+        case Some("challenge") => ChessLogger.debug(s"received challenge ${event.challenge.getOrElse("ex get chal")}"); processChallenge(event.challenge.get)
+        case Some("gameStart") => ChessLogger.debug("game started"); processGame(event.game.get.id)
+        case Some("gameFinish") => ChessLogger.debug("game finished"); resetEndpoint();
+        case Some("gameAborted") => ChessLogger.debug("game aborted"); resetEndpoint(); //TODO
+        case _ => ChessLogger.warn("event not recognized")
       }
     }
   }
 
+  private def resetEndpoint(): Unit ={
+    activeGame.set(false)
+    joinedGame.set(false)
+    gameStateExists.set(false)
+  }
+
   private def processChallenge(chal: Challenge): Unit ={ //TODO handle multiple challenges
-    println("processing challenge")
-    println(chal.challenger)
+    ChessLogger.trace("processing challenge")
+    ChessLogger.trace(chal.challenger.toString())
     if(chal.variant.key.equals("standard") && chal.timeControl.`type`.equals("clock") && chal.timeControl.limit >= 900){
       if(!activeGame.getAndSet(true)){
         Http(s"${server}/api/challenge/${chal.id}/accept").header("Authorization", s"Bearer $token").postForm.asString.code match {
-          case 200 => println("accepted challenge")
-          case 404 => println("challenge not found"); activeGame.set(false)
-          case 429 => println("Rate limit"); Thread.sleep(60000)
-          case _ => println("error accepting challenge"); activeGame.set(false)
+          case 200 => ChessLogger.debug("accepted challenge")
+          case 404 => ChessLogger.warn("challenge not found"); activeGame.set(false)
+          case 429 => ChessLogger.warn("Rate limit"); Thread.sleep(60000)
+          case _ => ChessLogger.error("error accepting challenge"); activeGame.set(false)
         }
       }
     } else {
       Http(s"${server}/api/challenge/${chal.id}/decline").header("Authorization", s"Bearer $token").postForm.asString.code match {
-        case 200 => println("declined challenge")
-        case 404 => println("no challenge to decline")
-        case 429 => println("Rate limit"); Thread.sleep(60000)
-        case _ => println("error declining challenge")
+        case 200 => ChessLogger.debug("declined challenge")
+        case 404 => ChessLogger.warn("no challenge to decline")
+        case 429 => ChessLogger.warn("Rate limit"); Thread.sleep(60000)
+        case _ => ChessLogger.error("error declining challenge")
       }
     }
   }
 
   private def processGame(gameId: String): Unit ={
-    println(s"joining game $gameId")
+    ChessLogger.info(s"joining game $gameId")
     activeGame.set(true)
     joinedGame.set(true)
     this.gameId = gameId
@@ -106,15 +123,15 @@ class LichessEndpoint(token: String, botId: String, server: String) extends Mode
           strm => {
             scala.io.Source.fromInputStream(strm).getLines().foreach(processGameState)
           }).code match {
-          case 200 => println("event received OK")
-          case 429 => println("rate limit req"); Thread.sleep(60000)
-          case _ => println("error - other status code")
+          case 200 => ChessLogger.debug("event received OK")
+          case 429 => ChessLogger.warn("rate limit req"); Thread.sleep(60000)
+          case _ => ChessLogger.error("error - other status code")
         }
       } catch {
-        case ex: Exception => println("Game Processing Error: " + ex.getMessage)
+        case ex: Exception => ChessLogger.error("Game Processing Error: " + ex.getMessage)
       }
     }
-    println("Game stream closed normally")
+    ChessLogger.debug("Game stream closed normally")
   }
 
   private def processGameState(state: String): Unit ={
@@ -132,14 +149,14 @@ class LichessEndpoint(token: String, botId: String, server: String) extends Mode
           case Right(ges) => println(ges); processGameEventState(ges)
         }
       } catch {
-        case _ => println("chat or unknown game state")
+        case _ => ChessLogger.debug("chat or unknown game state")
       }
     }
   }
 
   private def processFullGame(gf: GameFull): Unit ={
     if(!gameStateExists.get){
-      println(if(gf.white.nonEmpty) gf.white.get.id else "anon" + " opp")
+      ChessLogger.debug(if(gf.white.nonEmpty) gf.white.get.id else "anon" + " opp")
       opponentColor = if((if(gf.white.nonEmpty) gf.white.get.id else "anon").equals(botId)) "black" else "white"
       gf.initialFen match {
         case "startpos" =>
@@ -150,42 +167,40 @@ class LichessEndpoint(token: String, botId: String, server: String) extends Mode
           gameState = GameCommands.startGame(Vector(opponentColor, s), Some(this)).get
           engine = new AlphaBetaEngine(gameState, !opponentColor.equals("white"))
           gameStateExists.set(true)
-        case _ => println("invalid fen received")
+        case _ => ChessLogger.error("invalid fen received")
       }
-    }
-    if(!gf.state.status.equals("started")){
-      println(s"Game ${gf.state.status}")
-      joinedGame.set(false)
-      activeGame.set(false)
-    } else {
-      processGameEventState(gf.state)
     }
   }
 
   private def processGameEventState(ges: GameEventState): Unit ={
-    val moves = ges.moves.split(" ")
-    val whoseTurn = moves.length % 2
-    lazy val move = moves.last
-    lazy val from = move.take(2)
-    lazy val to = move.drop(2).take(2)
-    lazy val prom = move.drop(4).take(1)
-    println(s"turn $whoseTurn, opp is $opponentColor")
-    if(whoseTurn == 1){
-      println("turn of 1")
-      if(opponentColor.equals("white")){
-        println(s"adding opp (W) move $from $to $prom")
-        if(gameState.makeMove(from, to, prom).nonEmpty){
-          engine.makeMove()
+    if(ges.status.equals("started")) {
+      val moves = ges.moves.split(" ")
+      val whoseTurn = moves.length % 2
+      lazy val move = moves.last
+      lazy val from = move.take(2)
+      lazy val to = move.drop(2).take(2)
+      lazy val prom = move.drop(4).take(1)
+      ChessLogger.trace(s"turn $whoseTurn, opp is $opponentColor")
+      if (whoseTurn == 1) {
+        ChessLogger.trace("turn of 1")
+        if (opponentColor.equals("white")) {
+          ChessLogger.trace(s"adding opp (W) move $from $to $prom")
+          if (gameState.makeMove(from, to, prom).nonEmpty) {
+            engine.makeMove()
+          }
+        }
+      } else {
+        ChessLogger.trace("turn of 0")
+        if (opponentColor.equals("black")) {
+          ChessLogger.trace(s"adding opp (B) move $from $to $prom")
+          if (gameState.makeMove(from, to, prom).nonEmpty) {
+            engine.makeMove()
+          }
         }
       }
     } else {
-      println("turn of 0")
-      if(opponentColor.equals("black")){
-        println(s"adding opp (B) move $from $to $prom")
-        if(gameState.makeMove(from, to, prom).nonEmpty){
-          engine.makeMove()
-        }
-      }
+      ChessLogger.debug(s"Game ${ges.status}")
+      resetEndpoint()
     }
   }
 
@@ -194,9 +209,9 @@ class LichessEndpoint(token: String, botId: String, server: String) extends Mode
     if(!move.situationBefore.color.name.equals(opponentColor)){
       val response: HttpResponse[String] = Http(s"${server}/api/bot/game/${gameId}/move/${uci}").header("Authorization", s"Bearer $token").postForm.asString
       response.code match {
-        case 200 => println(s"Move Success: ${response.body}")
-        case 429 => println("Rate limit"); Thread.sleep(60000); sendMove(move)
-        case _ => println(s"Move Error: ${response.body}")
+        case 200 => ChessLogger.debug(s"Move Success: ${response.body}")
+        case 429 => ChessLogger.warn("Rate limit"); Thread.sleep(60000); sendMove(move)
+        case _ => ChessLogger.error(s"Move Error: ${response.body}")
       }
     }
   }
--- src/main/scala/edu/brown/cs/main/Main.scala
+++ src/main/scala/edu/brown/cs/main/Main.scala
@@ -1,7 +1,7 @@
 package edu.brown.cs.main
 
 import edu.brown.cs.chessgame.{GameCommands, GameState}
-import edu.brown.cs.io.REPL
+import edu.brown.cs.io.{ChessLogger, REPL}
 import edu.brown.cs.io.lichess.LichessEndpoint
 import edu.brown.cs.uci.EngineCommands
 
@@ -21,21 +21,32 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val ep = new LichessEndpoint(token, botId, server)
-    val commandMap: mutable.HashMap[String, Vector[String] => Any] = mutable.HashMap(
-      "greet" -> GeneralCommands.hello,
-      "startgame" -> GameCommands.startGame,
-      "move" -> GameCommands.makeMove,
-      "bestmove" -> GameCommands.makeMove,
-      "evaluate" -> GameCommands.evaluate,
-      "depth" -> GameCommands.depth,
-      "upgrade" -> ep.upgradeToBot,
-      "connect" -> ep.streamEvents,
-      "engine" -> EngineCommands.startEngine,
-      "stopengine" -> EngineCommands.stopEngine,
-      "uci" -> EngineCommands.updateUciStream
-    )
+    if(args.size == 0){ //default - run with Lichess bot only
+      ChessLogger.info("Initializing in bot-only mode.")
+      ep.streamEvents(Vector[String]())
+      while(ep.isConnectionOpen()){
 
-    val repl = new REPL(commandMap)
-    repl.run()
+      }
+    } else { // run with a REPL in terminal
+      if(args(0).toLowerCase().equals("repl")){
+        val commandMap: mutable.HashMap[String, Vector[String] => Any] = mutable.HashMap(
+          "greet" -> GeneralCommands.hello,
+          "startgame" -> GameCommands.startGame,
+          "move" -> GameCommands.makeMove,
+          "bestmove" -> GameCommands.makeMove,
+          "evaluate" -> GameCommands.evaluate,
+          "depth" -> GameCommands.depth,
+          "upgrade" -> ep.upgradeToBot,
+          "connect" -> ep.streamEvents,
+          "engine" -> EngineCommands.startEngine,
+          "stopengine" -> EngineCommands.stopEngine,
+          "uci" -> EngineCommands.updateUciStream
+        )
+        val repl = new REPL(commandMap)
+        repl.run()
+      } else {
+        ChessLogger.error("Unrecognized argument.")
+      }
+    }
   }
 }
